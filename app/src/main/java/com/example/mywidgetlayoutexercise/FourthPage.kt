656b8815c1d48b2aba47da51fa9ceebdf6690128
Beratcan Güreş <guresberatcan@gmail.com>
2021-06-22 12:48:07 +0300

    Third Page Finished

---
+++ app/src/main/java/com/example/mywidgetlayoutexercise/FourthPage.kt
@@ -0,0 +1,27 @@
+package com.example.mywidgetlayoutexercise
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
+class FourthPage : AppCompatActivity() {
+    //lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fourth)
+
+
+/*
+        button.setOnClickListener {
+            val intent = Intent(this, ThirdPage::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+*/
+    }
+}
--- app/src/main/java/com/example/mywidgetlayoutexercise/ThirdPage.kt
+++ app/src/main/java/com/example/mywidgetlayoutexercise/ThirdPage.kt
@@ -2,17 +2,60 @@ package com.example.mywidgetlayoutexercise
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class ThirdPage : AppCompatActivity() {
+    lateinit var listView: ListView
+    lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
+        listView = findViewById(R.id.listview)
+        button = findViewById(R.id.third_page_button)
+
+        val names = arrayOf(
+            "Test1",
+            "Test2",
+            "Test3",
+            "Test4",
+            "Test5",
+            "Test6",
+            "Test7",
+            "Test8",
+            "Test9",
+            "Test10",
+            "Test11",
+            "Test12",
+            "Test13",
+            "Test14",
+            "Test15",
+            "Test16",
+            "Test17",
+            "Test18",
+            "Test19",
+            "Test20"
+        )
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1, names
+        )
+        listView.adapter = arrayAdapter
+
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            Toast.makeText(
+                this,
+                "Item Selected " + names[i],
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        button.setOnClickListener {
+            val intent = Intent(this, FourthPage::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
